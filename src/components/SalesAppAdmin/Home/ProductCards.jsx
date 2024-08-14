bf8923d260208bfabed22561193ca996dc8124ec
Vladimir Sulovic <sulovic@gmail.com>
2024-08-14 23:26:49 +0200

    Created Home page with products

--- src/components/SalesAppAdmin/Home/ProductCards.jsx
+++ src/components/SalesAppAdmin/Home/ProductCards.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { parse } from "postcss";
+import React from "react";
 
-const ProductCards = ({product}) => {
+const ProductCards = ({ product }) => {
+  const apiUrl = import.meta.env.VITE_APP_API_BASE_URL;
+
   return (
-    <div>ProductCards</div>
-  )
-}
+    <div className=" border-solid border-2 rounded-lg p-4 gap-2 border-gray-200 shadow-xl grid grid-cols-1 justify-items-center text-center">
+      <div className="h-14 flex items-center ">
+        <h4>{product?.productName}</h4>
+      </div>
+      <div className="w-full h-0.5 bg-zinc-400"></div>
 
-export default ProductCards
+      <div className="h-64 md:h-96 flex items-center justify-center">
+        <img src={product?.productImage ? apiUrl + "/" + product?.productImage : "/placeholder.png"} alt="productImage" className="h-full object-contain" />
+      </div>
+      <div className="w-full h-0.5 bg-zinc-400"></div>
+
+      <p>{product?.productDesc}</p>
+      <p>BC: {product?.productBarcode}</p>
+      <h5>Cena: {parseFloat(product?.regularPrice).toLocaleString("sr-Latn-RS", { minimumFractionDigits: 2, maximumFractionDigits: 2 })} RSD</h5>
+    </div>
+  );
+};
+
+export default ProductCards;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,52 @@
-import React from "react";
-import { useAuth } from "../Context/AuthContext";
+import React, { useEffect, useState } from "react";
+import Spinner from "../components/Spinner";
+import { toast } from "react-toastify";
+import ProductCards from "../components/SalesAppAdmin/Home/ProductCards";
+import axios from "axios";
 
 const Home = () => {
+  const [productsData, setProductsData] = useState(null);
+  const [showSpinner, setShowSpinner] = useState(false);
 
-  const { authUser, handleLogin } = useAuth();
+
+  const fetchProducts = async () => {
+    try {
+      setShowSpinner(true);
+      const productsResponse = await axios.get(`${import.meta.env.VITE_APP_API_BASE_URL}/api/productsPublic`);
+      setProductsData(productsResponse?.data);
+      console.log(productsResponse?.data);
+    } catch (err) {
+      toast.error(`Ups! Došlo je do greške: ${err}`, {
+        position: "top-center",
+      });
+    } finally {
+      setShowSpinner(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <>
-      <h3>Home, Hello</h3>
-      <button className="mx-4 button button-red" onClick={() => handleLogin({ type: "password", email: "sulovic@gmail.com", password: "123456789" })}>Login</button>
+          <div className="w-screen">
+
+      <h3 className="text-center my-4">Proizvodi na akciji</h3>
+      <div className="p-4">
+        <div className="grid grid-cols-2 lg:grid-cols-4 3xl:grid-cols-8 gap-4">
+
+          {productsData?.map((product) => (
+            <ProductCards key={product?.productId} product={product} />
+          ))}
+
+        </div>
+
+      </div>
+      </div>
+
+      {showSpinner && <Spinner />}
+
     </>
   );
 };
